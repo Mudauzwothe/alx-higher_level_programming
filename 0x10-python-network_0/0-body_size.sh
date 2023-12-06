9a2e8a014d82888ac6d440038c2af46bb25d2224
Mudauzwothe <mudauzwothe@gmail.com>
2023-12-06 15:37:44 -0800

    hope

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,22 +1,3 @@
 #!/bin/bash
-
-# Check if the number of parameters is correct
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 URL"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Use curl to send a request to the URL and display the size of the body in bytes
-size=$(curl -sI "$url" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
-
-# Check if the size is a number
-if [[ "$size" =~ ^[0-9]+$ ]]; then
-    echo "$size"
-else
-    echo "Error: Unable to determine the size of the body."
-    exit 1
-fi
-
+#Send a request to that URL, and displays the size of the body of the response
+curl -s "$1" | wc -c
